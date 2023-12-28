3f1a5a22f3411e53b5799112357f59181696ed1d
Roddy Taipe <rodtai2019@gmail.com>
2023-12-28 12:51:15 -0800

    fix: Use a player input queue instead of applying player inputs directly on game

--- src/main/java/com/battleclub/gameserver/PlayerInputController.java
+++ src/main/java/com/battleclub/gameserver/PlayerInputController.java
@@ -5,45 +5,17 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
-import com.battleclub.gameserver.Game.Coordinates;
-import com.battleclub.gameserver.Game.Player;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @Slf4j
 public class PlayerInputController {
     @Autowired
-    private Game game;
-
-    @Autowired
-    private Users users;
-
-    @Autowired
-    private PlayerMovement playerMovement;
-
-    @Autowired
-    private CoordinateUtils coordinateUtils;
-
-    @Autowired
-    private WeaponSelection weaponSelection;
+    private PlayerInputManager playerInputManager;
 
     @MessageMapping("/accept_input")
     public void acceptInput(PlayerInputMessage playerInputMessage, @Header("simpSessionId") String sessionId) {
-        String userId = users.getUsers().get(sessionId).userId();
-        Player oldPlayerValue = game.players.get(userId);
-        if (oldPlayerValue != null) {
-            // net movement is calculated from player input keys selected
-            Coordinates netMovement = playerMovement.calculateNetMovement(playerInputMessage.getKeysSelected());
-            Coordinates newCoords = coordinateUtils.add(netMovement, oldPlayerValue.coords());
-            int inputWeaponIdx = weaponSelection.getWeaponIndex(playerInputMessage.getKeysSelected());
-            // if -1, then retain old weapon idx else use input weapon idx
-            int newWeaponIdx = inputWeaponIdx == -1 ? oldPlayerValue.weaponIdx() : inputWeaponIdx;
-            // new player value is composed of net movement + old player coords and player input angle
-            Player newPlayerValue = new Player(userId, oldPlayerValue.health(), playerInputMessage.getAngle(), newCoords, newWeaponIdx);
-            // records are immutable so we need to copy over all old values, merge them with new ones and then add back into concurrent map
-            game.players.put(userId, newPlayerValue);
-        }
+        playerInputManager.push(playerInputMessage, sessionId);
     }
 
 }
---
+++ src/main/java/com/battleclub/gameserver/PlayerInputManager.java
@@ -0,0 +1,40 @@
+package com.battleclub.gameserver;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PlayerInputManager {
+    public record PlayerInput (PlayerInputMessage playerInputMessage, String sessionId) {}
+    private List<PlayerInput> playerInputs;
+    private final Lock lock;
+
+    public PlayerInputManager() {
+        this.playerInputs = new ArrayList<>();
+        this.lock = new ReentrantLock();
+    }
+
+    public void push(PlayerInputMessage playerInputMessage, String sessionId) {
+        lock.lock();
+        try {
+            playerInputs.add(new PlayerInput(playerInputMessage, sessionId));
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public List<PlayerInput> popAll() {
+        lock.lock();
+        try {
+            List<PlayerInput> toReturn = playerInputs;
+            playerInputs = new ArrayList<>();
+            return toReturn;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
